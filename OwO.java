1a25f026ec924a7317e9995b31b3acc590f1f4d5
AngelKiba <66314936+AngelKiba@users.noreply.github.com>
2020-08-28 13:35:56 -0400

    Update OwO.java

--- OwO.java
+++ OwO.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class OwO{
+  
+  private String name;
+  
+  public OwO(String name1){
+    this.name = name1;
+  }
+  
+  public String getName(){
+    return name;
+  }
+ 
+}
